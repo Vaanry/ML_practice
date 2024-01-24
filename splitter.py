46faacccb90b468f908e8f1e82591b53037cca4f
Yulia <darkstarscream123@gmail.com>
2024-01-24 17:32:10 +0700

    splitter

---
+++ splitter.py
@@ -0,0 +1,422 @@
+"""GroupTimeSeriesSplit and PurgedGroupTimeSeriesSplit from Kaggle Notebook: https://www.kaggle.com/code/konradb/ts-10-validation-methods-for-time-series"""
+
+
+from collections.abc import Iterable
+from collections import defaultdict
+import warnings
+from itertools import chain, combinations
+from math import ceil, floor
+import numbers
+from abc import ABCMeta, abstractmethod
+from inspect import signature
+
+import numpy as np
+from scipy.special import comb
+
+from sklearn.utils import indexable, check_random_state, _safe_indexing
+from sklearn.utils import _approximate_mode
+from sklearn.utils.validation import _num_samples, column_or_1d
+from sklearn.utils.validation import check_array
+from sklearn.utils.multiclass import type_of_target
+from sklearn.base import _pprint
+from sklearn.model_selection._split import _BaseKFold, indexable, _num_samples
+from sklearn.utils.validation import _deprecate_positional_args
+
+class BaseCrossValidator(metaclass=ABCMeta):
+    """Base class for all cross-validators
+
+    Implementations must define `_iter_test_masks` or `_iter_test_indices`.
+    """
+
+    def split(self, X, y=None, groups=None):
+        """Generate indices to split data into training and test set.
+
+        Parameters
+        ----------
+        X : array-like of shape (n_samples, n_features)
+            Training data, where `n_samples` is the number of samples
+            and `n_features` is the number of features.
+
+        y : array-like of shape (n_samples,)
+            The target variable for supervised learning problems.
+
+        groups : array-like of shape (n_samples,), default=None
+            Group labels for the samples used while splitting the dataset into
+            train/test set.
+
+        Yields
+        ------
+        train : ndarray
+            The training set indices for that split.
+
+        test : ndarray
+            The testing set indices for that split.
+        """
+        X, y, groups = indexable(X, y, groups)
+        indices = np.arange(_num_samples(X))
+        for test_index in self._iter_test_masks(X, y, groups):
+            train_index = indices[np.logical_not(test_index)]
+            test_index = indices[test_index]
+            yield train_index, test_index
+
+    # Since subclasses must implement either _iter_test_masks or
+    # _iter_test_indices, neither can be abstract.
+    def _iter_test_masks(self, X=None, y=None, groups=None):
+        """Generates boolean masks corresponding to test sets.
+
+        By default, delegates to _iter_test_indices(X, y, groups)
+        """
+        for test_index in self._iter_test_indices(X, y, groups):
+            test_mask = np.zeros(_num_samples(X), dtype=bool)
+            test_mask[test_index] = True
+            yield test_mask
+
+    def _iter_test_indices(self, X=None, y=None, groups=None):
+        """Generates integer indices corresponding to test sets."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_n_splits(self, X=None, y=None, groups=None):
+        """Returns the number of splitting iterations in the cross-validator"""
+
+    def __repr__(self):
+        return _build_repr(self)
+        
+    
+class _BaseKFold(BaseCrossValidator, metaclass=ABCMeta):
+    """Base class for KFold, GroupKFold, and StratifiedKFold"""
+
+    @abstractmethod
+    def __init__(self, n_splits, *, shuffle, random_state):
+        if not isinstance(n_splits, numbers.Integral):
+            raise ValueError(
+                "The number of folds must be of Integral type. "
+                "%s of type %s was passed." % (n_splits, type(n_splits))
+            )
+        n_splits = int(n_splits)
+
+        if n_splits <= 1:
+            raise ValueError(
+                "k-fold cross-validation requires at least one"
+                " train/test split by setting n_splits=2 or more,"
+                " got n_splits={0}.".format(n_splits)
+            )
+
+        if not isinstance(shuffle, bool):
+            raise TypeError("shuffle must be True or False; got {0}".format(shuffle))
+
+        if not shuffle and random_state is not None:  # None is the default
+            raise ValueError(
+                "Setting a random_state has no effect since shuffle is "
+                "False. You should leave "
+                "random_state to its default (None), or set shuffle=True.",
+            )
+
+        self.n_splits = n_splits
+        self.shuffle = shuffle
+        self.random_state = random_state
+
+    def split(self, X, y=None, groups=None):
+        """Generate indices to split data into training and test set.
+
+        Parameters
+        ----------
+        X : array-like of shape (n_samples, n_features)
+            Training data, where `n_samples` is the number of samples
+            and `n_features` is the number of features.
+
+        y : array-like of shape (n_samples,), default=None
+            The target variable for supervised learning problems.
+
+        groups : array-like of shape (n_samples,), default=None
+            Group labels for the samples used while splitting the dataset into
+            train/test set.
+
+        Yields
+        ------
+        train : ndarray
+            The training set indices for that split.
+
+        test : ndarray
+            The testing set indices for that split.
+        """
+        X, y, groups = indexable(X, y, groups)
+        n_samples = _num_samples(X)
+        if self.n_splits > n_samples:
+            raise ValueError(
+                (
+                    "Cannot have number of splits n_splits={0} greater"
+                    " than the number of samples: n_samples={1}."
+                ).format(self.n_splits, n_samples)
+            )
+
+        for train, test in super().split(X, y, groups):
+            yield train, test
+
+    def get_n_splits(self, X=None, y=None, groups=None):
+        """Returns the number of splitting iterations in the cross-validator
+
+        Parameters
+        ----------
+        X : object
+            Always ignored, exists for compatibility.
+
+        y : object
+            Always ignored, exists for compatibility.
+
+        groups : object
+            Always ignored, exists for compatibility.
+
+        Returns
+        -------
+        n_splits : int
+            Returns the number of splitting iterations in the cross-validator.
+        """
+        return self.n_splits
+                      
+class GroupTimeSeriesSplit(_BaseKFold):
+    """Time Series cross-validator variant with non-overlapping groups.
+    Provides train/test indices to split time series data samples
+    that are observed at fixed time intervals according to a
+    third-party provided group.
+    In each split, test indices must be higher than before, and thus shuffling
+    in cross validator is inappropriate.
+    This cross-validation object is a variation of :class:`KFold`.
+    In the kth split, it returns first k folds as train set and the
+    (k+1)th fold as test set.
+    The same group will not appear in two different folds (the number of
+    distinct groups has to be at least equal to the number of folds).
+    Note that unlike standard cross-validation methods, successive
+    training sets are supersets of those that come before them.
+    Read more in the :ref:`User Guide <cross_validation>`.
+    Parameters
+    ----------
+    n_splits : int, default=5
+        Number of splits. Must be at least 2.
+    max_train_size : int, default=None
+        Maximum size for a single training set.
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from sklearn.model_selection import GroupTimeSeriesSplit
+    >>> groups = np.array(['a', 'a', 'a', 'a', 'a', 'a',\
+                           'b', 'b', 'b', 'b', 'b',\
+                           'c', 'c', 'c', 'c',\
+                           'd', 'd', 'd'])
+    >>> gtss = GroupTimeSeriesSplit(n_splits=3)
+    >>> for train_idx, test_idx in gtss.split(groups, groups=groups):
+    ...     print("TRAIN:", train_idx, "TEST:", test_idx)
+    ...     print("TRAIN GROUP:", groups[train_idx],\
+                  "TEST GROUP:", groups[test_idx])
+    TRAIN: [0, 1, 2, 3, 4, 5] TEST: [6, 7, 8, 9, 10]
+    TRAIN GROUP: ['a' 'a' 'a' 'a' 'a' 'a']\
+    TEST GROUP: ['b' 'b' 'b' 'b' 'b']
+    TRAIN: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10] TEST: [11, 12, 13, 14]
+    TRAIN GROUP: ['a' 'a' 'a' 'a' 'a' 'a' 'b' 'b' 'b' 'b' 'b']\
+    TEST GROUP: ['c' 'c' 'c' 'c']
+    TRAIN: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]\
+    TEST: [15, 16, 17]
+    TRAIN GROUP: ['a' 'a' 'a' 'a' 'a' 'a' 'b' 'b' 'b' 'b' 'b' 'c' 'c' 'c' 'c']\
+    TEST GROUP: ['d' 'd' 'd']
+    """
+    @_deprecate_positional_args
+    def __init__(self,
+                 n_splits=5,
+                 *,
+                 max_train_size=None
+                 ):
+        super().__init__(n_splits, shuffle=False, random_state=None)
+        self.max_train_size = max_train_size
+
+    def split(self, X, y=None, groups=None):
+        """Generate indices to split data into training and test set.
+        Parameters
+        ----------
+        X : array-like of shape (n_samples, n_features)
+            Training data, where n_samples is the number of samples
+            and n_features is the number of features.
+        y : array-like of shape (n_samples,)
+            Always ignored, exists for compatibility.
+        groups : array-like of shape (n_samples,)
+            Group labels for the samples used while splitting the dataset into
+            train/test set.
+        Yields
+        ------
+        train : ndarray
+            The training set indices for that split.
+        test : ndarray
+            The testing set indices for that split.
+        """
+        if groups is None:
+            raise ValueError(
+                "The 'groups' parameter should not be None")
+        X, y, groups = indexable(X, y, groups)
+        n_samples = _num_samples(X)
+        n_splits = self.n_splits
+        n_folds = n_splits + 1
+        group_dict = {}
+        u, ind = np.unique(groups, return_index=True)
+        unique_groups = u[np.argsort(ind)]
+        n_samples = _num_samples(X)
+        n_groups = _num_samples(unique_groups)
+        for idx in np.arange(n_samples):
+            if (groups[idx] in group_dict):
+                group_dict[groups[idx]].append(idx)
+            else:
+                group_dict[groups[idx]] = [idx]
+        if n_folds > n_groups:
+            raise ValueError(
+                ("Cannot have number of folds={0} greater than"
+                 " the number of groups={1}").format(n_folds,
+                                                     n_groups))
+        group_test_size = n_groups // n_folds
+        group_test_starts = range(n_groups - n_splits * group_test_size,
+                                  n_groups, group_test_size)
+        for group_test_start in group_test_starts:
+            train_array = []
+            test_array = []
+            for train_group_idx in unique_groups[:group_test_start]:
+                train_array_tmp = group_dict[train_group_idx]
+                train_array = np.sort(np.unique(
+                                      np.concatenate((train_array,
+                                                      train_array_tmp)),
+                                      axis=None), axis=None)
+            train_end = train_array.size
+            if self.max_train_size and self.max_train_size < train_end:
+                train_array = train_array[train_end -
+                                          self.max_train_size:train_end]
+            for test_group_idx in unique_groups[group_test_start:
+                                                group_test_start +
+                                                group_test_size]:
+                test_array_tmp = group_dict[test_group_idx]
+                test_array = np.sort(np.unique(
+                                              np.concatenate((test_array,
+                                                              test_array_tmp)),
+                                     axis=None), axis=None)
+            yield [int(i) for i in train_array], [int(i) for i in test_array] 
+            
+                      
+                      
+class PurgedGroupTimeSeriesSplit(_BaseKFold):
+    """Time Series cross-validator variant with non-overlapping groups.
+    Allows for a gap in groups to avoid potentially leaking info from
+    train into test if the model has windowed or lag features.
+    Provides train/test indices to split time series data samples
+    that are observed at fixed time intervals according to a
+    third-party provided group.
+    In each split, test indices must be higher than before, and thus shuffling
+    in cross validator is inappropriate.
+    This cross-validation object is a variation of :class:`KFold`.
+    In the kth split, it returns first k folds as train set and the
+    (k+1)th fold as test set.
+    The same group will not appear in two different folds (the number of
+    distinct groups has to be at least equal to the number of folds).
+    Note that unlike standard cross-validation methods, successive
+    training sets are supersets of those that come before them.
+    Read more in the :ref:`User Guide <cross_validation>`.
+    Parameters
+    ----------
+    n_splits : int, default=5
+        Number of splits. Must be at least 2.
+    max_train_group_size : int, default=Inf
+        Maximum group size for a single training set.
+    group_gap : int, default=None
+        Gap between train and test
+    max_test_group_size : int, default=Inf
+        We discard this number of groups from the end of each train split
+    """
+
+    @_deprecate_positional_args
+    def __init__(self,
+                 n_splits=5,
+                 *,
+                 max_train_group_size=np.inf,
+                 max_test_group_size=np.inf,
+                 group_gap=None,
+                 verbose=False
+                 ):
+        super().__init__(n_splits, shuffle=False, random_state=None)
+        self.max_train_group_size = max_train_group_size
+        self.group_gap = group_gap
+        self.max_test_group_size = max_test_group_size
+        self.verbose = verbose
+
+    def split(self, X, y=None, groups=None):
+        """Generate indices to split data into training and test set.
+        Parameters
+        ----------
+        X : array-like of shape (n_samples, n_features)
+            Training data, where n_samples is the number of samples
+            and n_features is the number of features.
+        y : array-like of shape (n_samples,)
+            Always ignored, exists for compatibility.
+        groups : array-like of shape (n_samples,)
+            Group labels for the samples used while splitting the dataset into
+            train/test set.
+        Yields
+        ------
+        train : ndarray
+            The training set indices for that split.
+        test : ndarray
+            The testing set indices for that split.
+        """
+        if groups is None:
+            raise ValueError(
+                "The 'groups' parameter should not be None")
+        X, y, groups = indexable(X, y, groups)
+        n_samples = _num_samples(X)
+        n_splits = self.n_splits
+        group_gap = self.group_gap
+        max_test_group_size = self.max_test_group_size
+        max_train_group_size = self.max_train_group_size
+        n_folds = n_splits + 1
+        group_dict = {}
+        u, ind = np.unique(groups, return_index=True)
+        unique_groups = u[np.argsort(ind)]
+        n_samples = _num_samples(X)
+        n_groups = _num_samples(unique_groups)
+        for idx in np.arange(n_samples):
+            if (groups[idx] in group_dict):
+                group_dict[groups[idx]].append(idx)
+            else:
+                group_dict[groups[idx]] = [idx]
+        if n_folds > n_groups:
+            raise ValueError(
+                ("Cannot have number of folds={0} greater than"
+                 " the number of groups={1}").format(n_folds,
+                                                     n_groups))
+
+        group_test_size = min(n_groups // n_folds, max_test_group_size)
+        group_test_starts = range(n_groups - n_splits * group_test_size,
+                                  n_groups, group_test_size)
+        for group_test_start in group_test_starts:
+            train_array = []
+            test_array = []
+
+            group_st = max(0, group_test_start - group_gap - max_train_group_size)
+            for train_group_idx in unique_groups[group_st:(group_test_start - group_gap)]:
+                train_array_tmp = group_dict[train_group_idx]
+                
+                train_array = np.sort(np.unique(
+                                      np.concatenate((train_array,
+                                                      train_array_tmp)),
+                                      axis=None), axis=None)
+
+            train_end = train_array.size
+ 
+            for test_group_idx in unique_groups[group_test_start:
+                                                group_test_start +
+                                                group_test_size]:
+                test_array_tmp = group_dict[test_group_idx]
+                test_array = np.sort(np.unique(
+                                              np.concatenate((test_array,
+                                                              test_array_tmp)),
+                                     axis=None), axis=None)
+
+            test_array  = test_array[group_gap:]
+            
+            
+            if self.verbose > 0:
+                    pass
+                    
+            yield [int(i) for i in train_array], [int(i) for i in test_array]
